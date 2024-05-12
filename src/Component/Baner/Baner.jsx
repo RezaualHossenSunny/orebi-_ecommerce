04137bb8e281c2ae3ffa428e45f799d387c9d3fd
RezaualHossenSunny <91420025+RezaualHossenSunny@users.noreply.github.com>
2024-05-12 12:36:59 +0600

    baner done

--- src/Component/Baner/Baner.jsx
+++ src/Component/Baner/Baner.jsx
@@ -1,15 +1,75 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import baner from '../../assets/baner.png';
 const Baner = () => {
+  const [activeDots, Setactivedots]=useState(0)
     const settings = {
         dots: true,
         infinite: true,
         speed: 500,
         slidesToShow: 1,
-        slidesToScroll: 1
+        slidesToScroll: 1,
+        autoplay:true,
+        beforeChange: (prev, next) => {
+          Setactivedots(next );
+        },
+        appendDots: dots => (
+          <div
+            style={{
+           position:"absolute",
+           top:"40%",
+           left:"150px"
+              
+            }}
+          >
+            <ul style={{ margin: "0px"  }}> 
+            {
+              dots.map((item)=>{
+                return <li style={{display:"block", marginTop:"10px"}}>{item} </li>
+              })
+
+              
+            }
+           
+             </ul>
+          </div>
+        ),
+        customPaging: i => (
+          <div
+            style={
+              i === activeDots ? 
+               {
+              width: "30px",
+              
+              borderRight: "3px #262626 solid",
+             paddingTop:"10px",
+             paddingBottom:"10px",
+             paddingRight:"30px",
+             fontFamily:"dm sans",
+             color:"#262626",
+             fontSize:"14px",
+             fontWeight:"400"
+            }
+               : 
+              {
+              width: "30px",
+            
+              borderRight: "3px white solid",
+             paddingTop:"10px",
+             paddingBottom:"10px",
+             paddingRight:"30px",
+             fontFamily:"dm sans",
+             color:"transparent",
+             fontSize:"14px",
+             fontWeight:"400"
+            }
+            }
+          >
+            0{i + 1}
+          </div>
+        )
       };
   return (
     <>
